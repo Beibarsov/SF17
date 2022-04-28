8bee4e35e8711dee427d539126aad49cdc2137b1
Beibarsov <BarsStudio@gmail.com>
2022-04-28 17:27:07 +0800

    Добавьте файлы проекта.

---
+++ SF17/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF17
+{
+    public static class Calculator
+    {
+        public static void CalculateInterest(ICalculatedAccount account)
+        {
+           account.CalculateInterest();
+        }
+    }
+}
---
+++ SF17/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SF17
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ICalculatedAccount account1 = new SalaryAccount { Balance = 20000};
+            ICalculatedAccount account2 = new SimpleAccount { Balance = 120000 };
+
+            Calculator.CalculateInterest(account1);
+            Calculator.CalculateInterest(account2);
+
+            Console.WriteLine("Процентная ставка зарплатного - " + account1.Interest);
+            Console.WriteLine("Процентная ставка обычного - " + account2.Interest);
+        }
+    }
+}
